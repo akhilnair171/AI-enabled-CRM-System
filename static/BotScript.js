b2201ebdbb44b031b037e3e8793135cfd9ebb29b
akhilnair171 <109281204+akhilnair171@users.noreply.github.com>
2023-05-28 12:40:40 +0530

    Add files via upload

---
+++ static/BotScript.js
@@ -0,0 +1,8 @@
+ (function(d, m){
+        var kommunicateSettings =
+            {"appId":"36e7d7c3ae0b1eeb8e43ff8000f46e94","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
